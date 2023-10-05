b00dba54d486fadcf87592ad829fff44703b6042
Alessandro <alessandro.colla@stevejobs.academy>
2023-10-05 09:44:31 +0200

    Upload file ultima volta modificati il 04-10-2023 18:36:38

---
+++ src/app/Persona.ts
@@ -0,0 +1,7 @@
+import { NomeCompleto } from "./NomeCompleto";
+import { DataDiNascita } from "./DataDiNascita";
+
+interface Persona{
+    nomeCompleto: NomeCompleto;
+    dataDiNascita: DataDiNascita;
+}
